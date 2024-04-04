b60c9f39773c554cc19cef792e12122a7bcc2a4d
Catherine Kieffer <kief4528@stthomas.edu>
2024-04-04 16:38:15 -0500

    catherine's changes

--- scripts/transforms.js
+++ scripts/transforms.js
@@ -2,27 +2,52 @@ import { Matrix, Vector } from "./matrix.js";
 
 // create a 4x4 matrix to the perspective projection / view matrix
 function mat4x4Perspective(prp, srp, vup, clip) {
-    // 1. translate PRP to origin
+    // 1. translate PRP to origin - Tpar
+    let TPar = mat4x4Translate(new Matrix(4,4), -prp.x, -prp.y, -prp.z);
+    
     // 2. rotate VRC such that (u,v,n) align with (x,y,z)
-    // 3. shear such that CW is on the z-axis
-    // 4. scale such that view volume bounds are ([z,-z], [z,-z], [-1,zmin])
+    let n = Vector.normalize(prp.subtract(srp));
+    let u = Vector.normalize(vup.cross(n));
+    let v = n.cross(u);
+    let R = [[u[0], u[1], u[2], 0],
+            [v[0], v[1], v[2], 0],
+            [n[0], n[1], n[2], 0],
+            [0,    0,     0,    0]];
 
-    // ...
-    // let transform = Matrix.multiply([...]);
-    // return transform;
+    // 3. shear such that CW is on the z-axis
+    let CW = [(clip[0]+clip[1])/2, (clip[2]+clip[3])/2, -1*clip[4]];
+    let DOP = CW;
+    let shxpar = (-1*DOP[0])/DOP[2];
+    let shypar = (-1*DOP[1])/DOP[2];
+    let SHpar = mat4x4ShearXY(new Matrix(4,4), shxpar, shypar);
+
+    // 4. scale such that view volume bounds are ([z,-z], [z,-z], [-1,zmin])
+    let sperx = (2*clip[4])/((clip[1]-clip[0])*clip[5]);
+    let spery = (2*clip[4])/((clip[3]-clip[2])*clip[5]);
+    let sperz = 1/clip[5];
+    let Sper = mat4x4Scale(new Matrix(4,4), sperx, spery, sperz);
+
+    let transform = Matrix.multiply([Sper, Tpar, SHpar, R, TPRP]);
+    return transform;
 }
 
 // create a 4x4 matrix to project a perspective image on the z=-1 plane
 function mat4x4MPer() {
     let mper = new Matrix(4, 4);
-    // mper.values = ...;
+    mper.values = [[1,0,0,0],
+                [0,1,0,0],
+                [0,0,1,0],
+                [0,0,-1,0]]  // d=-1 because of z plane 
     return mper;
 }
 
 // create a 4x4 matrix to translate/scale projected vertices to the viewport (window)
 function mat4x4Viewport(width, height) {
     let viewport = new Matrix(4, 4);
-    // viewport.values = ...;
+    viewport.values =  [[width/2,0,0,width/2],
+                    [0,height/2,0,height/2],
+                    [0,0,1,0],
+                    [0,0,0,1]] 
     return viewport;
 }
 
@@ -66,7 +91,10 @@ function mat4x4RotateZ(mat4x4, theta) {
 
 // set values of existing 4x4 matrix to the shear parallel to the xy-plane matrix
 function mat4x4ShearXY(mat4x4, shx, shy) {
-    // mat4x4.values = ...;
+    mat4x4.values = [[1, 0, shx, 0],
+    [0, 1, shy, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]];
 }
 
 // create a new 3-component vector with values x,y,z
